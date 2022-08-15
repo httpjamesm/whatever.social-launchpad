ad61e18b34c5460ec104a8eb39174ede8c435a2d
httpjamesm <github@httpjames.space>
2022-08-15 11:41:10 -0400

    add connect-src

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,8 @@ const config = {
 			mode: 'auto',
 			directives: {
 				'default-src': ['self'],
-                'img-src': ['self', '*.whatever.social']
+                'img-src': ['self', '*.whatever.social'],
+                'connect-src': ['self', 'statusapi.whatever.social'],
 			}
 		}
 	}
